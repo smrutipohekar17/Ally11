c28a670d6ee704b1218bd3a79642f7c00a12cc9b
rkp74 <rajankr8969@gmail.com>
2021-12-05 19:31:44 +0530

    github try

---
+++ git_Try/src/main/java/com/example/git_try/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.git_try;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Just try to work on github
+        setContentView(R.layout.activity_main);
+    }
+}
