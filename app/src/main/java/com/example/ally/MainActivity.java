9c7e4c9fcffb902e9b0fe5546217b2da9a0d7cf4
vranda25 <202052349@iiitvadodara.ac.in>
2021-12-13 13:20:20 +0530

    done some more changes in design by vranda

--- app/src/main/java/com/example/ally/MainActivity.java
+++ app/src/main/java/com/example/ally/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
