7f0f6f2b6ee74c88c87ef6ec08bd5054abc08f71
smrutipohekar17 <smrutipohekar17@gmail.com>
2021-12-22 18:12:07 +0530

    Personality Test Added

--- app/src/main/java/com/example/ally/MainActivity4.java
+++ app/src/main/java/com/example/ally/MainActivity4.java
@@ -19,4 +19,8 @@ public class MainActivity4 extends AppCompatActivity {
     public void nextActivity1(View view) {
         startActivity(new Intent(getApplicationContext(),MainActivity5.class));
     }
+
+    public void openTest(View view) {
+        startActivity(new Intent(getApplicationContext(),Test.class));
+    }
 }
