938d74f63dc51a68ed515427ae0b623216fada5d
vranda25 <202052349@iiitvadodara.ac.in>
2021-12-30 12:43:59 +0530

    by harshi

--- app/src/main/java/com/example/ally/Profile.java
+++ app/src/main/java/com/example/ally/Profile.java
@@ -1,8 +1,14 @@
 package com.example.ally;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main9);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.ex_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.editprofile) {
+            Toast.makeText(getApplicationContext(), "Edit Your Profile", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.logout) {
+            Toast.makeText(getApplicationContext(), "Logged Out", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
